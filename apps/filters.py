90a0c4ad3a60fbd5ebd1eb6d4a8e70c630a99a69
IaMuhammad <abduvaliyevmuhammad41@gmail.com>
2024-03-22 16:00:51 +0500

    fix order list

--- apps/filters.py
+++ apps/filters.py
@@ -1,9 +1,15 @@
 import django_filters
 
-from apps.models import Product
+from apps.models import Product, Order
 
 
 class ProductFilter(django_filters.FilterSet):
     class Meta:
         model = Product
         fields = ['category', ]
+
+
+class OrderFilter(django_filters.FilterSet):
+    class Meta:
+        model = Order
+        fields = ['status', 'color', 'size', 'product']
--- apps/views/shop/order.py
+++ apps/views/shop/order.py
@@ -1,7 +1,10 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_yasg.utils import swagger_auto_schema
+from rest_framework.filters import SearchFilter
 from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated
 
+from apps.filters import OrderFilter
 from apps.models import Order
 from apps.serializers.shop.order import OrderCreateModelSerializer, OrderListModelSerializer
 
@@ -19,6 +22,9 @@ class OrderCreateAPIView(CreateAPIView):
 class OrderListAPIView(ListAPIView):
     serializer_class = OrderListModelSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ['product__name']
+    filterset_class = OrderFilter
 
     def get_queryset(self):
         if self.request.user.is_authenticated:
