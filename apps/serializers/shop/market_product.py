f7b3eeb45b513817f6a58195b8ec51ee4f7573cf
IaMuhammad <abduvaliyevmuhammad41@gmail.com>
2024-03-27 15:01:55 +0500

    fix bugs

--- apps/serializers/shop/market_product.py
+++ apps/serializers/shop/market_product.py
@@ -4,6 +4,11 @@ from apps.models import Product
 
 
 class MarketProductListSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
-        fields = ('id', 'name', 'price', 'pay')
+        fields = ('id', 'name', 'price', 'pay', 'image')
+
+    def get_image(self, obj: Product):
+        return obj.get_image.image.url
